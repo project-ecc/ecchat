4eadd2a11e15c6ceb3c0f5743d7a7cd16a4ec386
Henry Young <henry@home-young.me.uk>
2020-10-30 21:35:10 +0000

    zmqeventloop implementation

--- zmqeventloop.py
+++ zmqeventloop.py
@@ -2,13 +2,19 @@
 # coding: UTF-8
 
 import urwid
+import heapq
+import time
 import zmq
+
+from itertools import count
 
 from urwid.main_loop import EventLoop
 
 ################################################################################
 
 class zmqEventLoop(EventLoop):
+
+	_alarm_break = count()
 
 	def __init__(self):
 
@@ -21,50 +27,121 @@ class zmqEventLoop(EventLoop):
 
 	def alarm(self, seconds, callback):
 
-		pass
+		handle = (time.time() + seconds, next(self._alarm_break), callback)
+		heapq.heappush(self._alarms, handle)
+		return handle
 
 	def remove_alarm(self, handle):
 
-		pass
+		try:
+			self._alarms.remove(handle)
+			heapq.heapify(self._alarms)
+			return True
+		except ValueError:
+			return False
+
 
 	def watch_queue(self, queue, callback, flags=1):
 
-		pass
+		if queue in self._queue_callbacks:
+			raise ValueError('already watching %r' % queue)
+		self._poller.register(queue, flags)
+		self._queue_callbacks[queue] = callback
+		return queue
 
-	def watch_file(self, fd, callback, flags=1):
-
-		pass
 
 	def remove_watch_queue(self, handle):
 
-		pass
+		try:
+			try:
+				self._poller.unregister(handle)
+			finally:
+				self._queue_callbacks.pop(handle, None)
+			return True
+		except KeyError:
+			return False
+
+
+	def watch_file(self, fd, callback, flags=1):
+
+		if isinstance(fd, int):
+			fd = os.fdopen(fd)
+		self._poller.register(fd, flags)
+		self._queue_callbacks[fd.fileno()] = callback
+		return fd
+
 
 	def remove_watch_file(self, handle):
 
-		pass
+		try:
+			try:
+				self._poller.unregister(handle)
+			finally:
+				self._queue_callbacks.pop(handle.fileno(), None)
+			return True
+		except KeyError:
+			return False
+
 
 	def enter_idle(self, callback):
 
-		pass
+		self._idle_handle += 1
+		self._idle_callbacks[self._idle_handle] = callback
+		return self._idle_handle
+
 
 	def remove_enter_idle(self, handle):
 
-		pass
+		try:
+			del self._idle_callbacks[handle]
+			return True
+		except KeyError:
+			return False
+
 
 	def _entering_idle(self):
 
-		pass
+		for callback in list(self._idle_callbacks.values()):
+			callback()
 
 	def run(self):
 
-		pass
+		try:
+			while True:
+				try:
+					self._loop()
+				except zmq.error.ZMQError as exc:
+					if exc.errno != errno.EINTR:
+						raise
+		except ExitMainLoop:
+			pass
 
 	def _loop(self):
 
-		pass
+		if self._alarms or self._did_something:
+			if self._alarms:
+				state = 'alarm'
+				timeout = max(0, self._alarms[0][0] - time.time())
+			if self._did_something and (not self._alarms or
+										(self._alarms and timeout > 0)):
+				state = 'idle'
+				timeout = 0
+			ready = dict(self._poller.poll(timeout))
+		else:
+			state = 'wait'
+			ready = dict(self._poller.poll())
 
-		
+		if not ready:
+			if state == 'idle':
+				self._entering_idle()
+				self._did_something = False
+			elif state == 'alarm':
+				due, tie_break, callback = heapq.heappop(self._alarms)
+				callback()
+				self._did_something = True
 
-
+		for queue, _ in ready.items():
+			self._queue_callbacks[queue]()
+			self._did_something = True
 
 ################################################################################
