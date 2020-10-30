ff7a304462dd322990f8356daf01c10af955f52d
Henry Young <henry@home-young.me.uk>
2020-10-30 10:50:15 +0000

    template for zmqEventLoop

--- zmqeventloop.py
+++ zmqeventloop.py
@@ -19,4 +19,52 @@ class zmqEventLoop(EventLoop):
 		self._idle_handle     = 0
 		self._idle_callbacks  = {}
 
+	def alarm(self, seconds, callback):
+
+		pass
+
+	def remove_alarm(self, handle):
+
+		pass
+
+	def watch_queue(self, queue, callback, flags=1):
+
+		pass
+
+	def watch_file(self, fd, callback, flags=1):
+
+		pass
+
+	def remove_watch_queue(self, handle):
+
+		pass
+
+	def remove_watch_file(self, handle):
+
+		pass
+
+	def enter_idle(self, callback):
+
+		pass
+
+	def remove_enter_idle(self, handle):
+
+		pass
+
+	def _entering_idle(self):
+
+		pass
+
+	def run(self):
+
+		pass
+
+	def _loop(self):
+
+		pass
+
+		
+
+
+
 ################################################################################
