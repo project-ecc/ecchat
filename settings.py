7f110fbbaef54e7b79a3ffbf747bf1f84fcb160f
Henry Young <henry@home-young.me.uk>
2021-02-04 16:49:59 +0000

    Added Dogecoin

--- settings.py
+++ settings.py
@@ -11,6 +11,11 @@ chains  = [{'coin_symbol' : 'ecc',
 			'rpc_user'    : 'litecoinrpc',
 			'rpc_pass'    : 'dowereallyneedpasswords123!!!'}]
 
+#		   {'coin_symbol' : 'doge',
+#			'rpc_address' : '192.168.1.41:22555',
+#			'rpc_user'    : 'dogecoinrpc',
+#			'rpc_pass'    : 'dowereallyneedpasswords123!!!'},
+
 #		   {'coin_symbol' : 'rdd',
 #			'rpc_address' : '127.0.0.1:45443',
 #			'rpc_user'    : 'reddcoinrpc',
