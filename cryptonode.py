4631caa71eb2a604dbdabfabc44c486bbc7e8854
Henry Young <henry@home-young.me.uk>
2021-04-25 22:28:37 +0000

    Another exception added for /send

--- cryptonode.py
+++ cryptonode.py
@@ -269,6 +269,10 @@ class eccoinNode(cryptoNode):
 
 			raise cryptoNodeException('Invalid amount')
 
+		except exc.RpcWalletError:
+
+			raise cryptoNodeException('Amount too small')
+
 		else:
 
 			return txid
