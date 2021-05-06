98a4dc876ccc0692b215be561531ae7dee72321f
Henry Young <henry@home-young.me.uk>
2021-05-06 21:57:51 +0000

    Removed node configs from settings.py

--- settings.py
+++ settings.py
@@ -1,22 +1,2 @@
 protocol_id  = 1
 protocol_ver = 1
-
-chains  = [{'coin_symbol' : 'ecc',
-			'rpc_address' : '127.0.0.1:19119',
-			'rpc_user'    : 'eccoinrpc',
-			'rpc_pass'    : 'ecc123-OK-Maybeitneedstobemorecomplex'},
-			
-		   {'coin_symbol' : 'ltc',
-			'rpc_address' : '127.0.0.1:9332',
-			'rpc_user'    : 'litecoinrpc',
-			'rpc_pass'    : 'dowereallyneedpasswords123!!!'}]
-
-#		   {'coin_symbol' : 'doge',
-#			'rpc_address' : '192.168.1.41:22555',
-#			'rpc_user'    : 'dogecoinrpc',
-#			'rpc_pass'    : 'dowereallyneedpasswords123!!!'},
-
-#		   {'coin_symbol' : 'rdd',
-#			'rpc_address' : '127.0.0.1:45443',
-#			'rpc_user'    : 'reddcoinrpc',
-#			'rpc_pass'    : 'dowereallyneedpasswords123!!!'}]
